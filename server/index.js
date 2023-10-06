48b1e46f9f24ec53b7c273c1d6660beb8a80b2ac
Sargaroth2511 <84723008+Sargaroth2511@users.noreply.github.com>
2023-10-06 16:07:54 +0200

    Connection to OpenAI added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,52 @@
 const path = require("path");
 const express = require("express");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
+const apiKey = process.env.MY_OPENAI_KEY;
 
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+// app.get("/api", (req, res) => {
+//   res.json({ message: "Hello from server!" });
+// });
+
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  // const userQuestion = req.body.question;
+
+  const options = {
+    method: "POST",
+    url: "https://api.openai.com/v1/chat/completions",
+    headers: {
+      // 'GPT-API-Key':process.env.REACT_APP_API_KEY,
+      // 'GPT-API-Host': 'api.openai.com/v1/chat/completions.com'
+      Authorization: `Bearer ${apiKey}`,
+    },
+    data: {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant. You answer Questions precisely and in a short way.",
+        },
+        { role: "user", content: "Wie hoch ist der Eiffelturm" },
+      ],
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      const assistantReply = response.data.choices[0].message.content;
+      res.json({ reply: assistantReply });
+    })
+    .catch(function (error) {
+      console.error("an error occured", error.response.data);
+    });
 });
 
 // All other GET requests not handled before will return our React app
